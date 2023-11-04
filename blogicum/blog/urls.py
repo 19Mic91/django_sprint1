494599cf25775cf0dbfd121f0e16aa8701fea417
Заровский Михаил <zarovskymihail.yandex.ru>
2023-11-04 13:50:58 +0300

    текст комментария

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -4,7 +4,7 @@ from  .views import index, post_detail, category_posts
 
 app_name = 'blog'
 
-urlpatterns: list = [
+urlpatterns = [
     path('', index, name='index'),
     path('<int:id>/', post_detail, name='post_detail'),
     path('<slug:category_slug>/', category_posts, name='category_posts'),
